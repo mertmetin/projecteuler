6786bdc15cc2d6cd5f346dfae78d9a1d5a6f08ed
mertmetin <mertmetin@outlook.com>
2014-03-22 11:52:01 -0400

    Sum of all numbers, less than one million, which are palindromic in base 10 and base 2.

--- ProjectEuler/src/36.py
+++ ProjectEuler/src/36.py
@@ -1,10 +1,10 @@
-LIMIT = 1000000
+limit = 1000000
 
 def is_palindrome(num):
     return num == num[::-1]
 
 sum = 0
-for i in range(1, LIMIT, 2):
+for i in range(1, limit, 2):
     if is_palindrome(str(i)) == True and is_palindrome(str(bin(i))[2:]):
         sum+= i
         
